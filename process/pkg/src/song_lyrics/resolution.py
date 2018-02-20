5da471069812f228ac329c8a15f43fc62e85d7ac
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-02-20 12:08:06 -0500

    fuzzy matching words in glove

--- process/pkg/src/song_lyrics/resolution.py
+++ process/pkg/src/song_lyrics/resolution.py
@@ -2,13 +2,13 @@
 Entity resolution between the words in the Musixmatch dataset and GLOVE
 """
 import logging
-
 from fuzzywuzzy import process
-
+from fuzzywuzzy import fuzz
 from song_lyrics.parse import load_json_data, glovetxt2dict
 
 logging.basicConfig(level=logging.INFO)
 
+logger = logging.getLogger(__name__)
 
 path_to_data = '../../data/clean/mxm_dataset.json'
 path_to_glove = '../../data/raw/glove.6B/glove.6B.50d.txt'
@@ -22,16 +22,16 @@ words, track_ids, bows = load_json_data(path_to_data)
 words_glove = set(glove.keys())
 words_data = set(words)
 
-logging.info('GLOVE contains {:,} words'.format(len(words_glove)))
-logging.info('Musixmatch contains {:,} words'.format(len(words_data)))
+logger.info('GLOVE contains {:,} words'.format(len(words_glove)))
+logger.info('Musixmatch contains {:,} words'.format(len(words_data)))
 
 # first find exact matches
 both = words_data & words_glove
 # now find the ones missing (we need to match this)
 to_match = words_data - both
 
-logging.info('Exact matches: {:,}'.format(len(both)))
-logging.info('To match: {:,}'.format(len(to_match)))
+logger.info('Exact matches: {:,}'.format(len(both)))
+logger.info('To match: {:,}'.format(len(to_match)))
 
 words_glove_sorted = sorted(list(words_glove))
 
@@ -47,7 +47,7 @@ def find_interval(words, start):
             break
 
     if start_idx is None:
-        raise ValueError("Couldn't find word starting with {}".format(start))
+        raise ValueError("Couldn't find word starting with '{}'".format(start))
 
     # find end index
     for idx, word in enumerate(words[start_idx + 1:]):
@@ -69,6 +69,10 @@ word = list(to_match)[0]
 word
 
 
-idxs = find_interval(words_glove_sorted, word[0])
+for word in list(to_match):
+    idxs = find_interval(words_glove_sorted, word[0])
 
-process.extractOne(word, words_glove_sorted[idxs])
+    match, score = process.extractOne(word, words_glove_sorted[idxs],
+                                      scorer=fuzz.ratio)
+
+    logger.info('Matched {} with {}, score: {}'.format(word, match, score))
