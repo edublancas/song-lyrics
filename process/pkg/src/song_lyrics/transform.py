fbd0ec91d7799e6a0f2b634d30eac5652986db64
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-02-20 11:21:20 -0500

    trying to convert to embeddings in parallel, option to only take the top words within the song

--- process/pkg/src/song_lyrics/transform.py
+++ process/pkg/src/song_lyrics/transform.py
@@ -1,4 +1,5 @@
 import logging
+from operator import itemgetter
 
 
 import pandas as pd
@@ -22,23 +23,48 @@ def bow2vector(bow, max_words):
     return vector
 
 
-def bow2embedding(bow, words, glove, max_words):
+def bow2embedding(bow, words, glove, max_words=None, global_max=False):
     """
     Replace counts in a bag of words representation with an embedding
     and convert it to a numpy vector
+
+    Parameters
+    ----------
+    bow: dict
+        Bag of words in {word_index: count} format
+
+    words: list
+        List of words
+
+    glove: dict
+        Word embeddings in {word: vector} format
+
+    max_words: int
+        Maximum number of words to consider
+
+    global_max: bool, optional
+        If True, max_words is determined by the popularity in the whole
+        corpus, that is, taking words[:max_words], if False max_words is
+        taken into account within each bag of words, defaults to False.
     """
+    # TODO: add featutre toselect  specific words and cutoff in bag of words
+
     (dim,) = list(glove.values())[0].shape
 
     vector = np.zeros(dim)
 
+    # sort bag of words
+    bow_sorted = sorted([(int(idx), count) for idx, count in bow.items()],
+                        key=itemgetter(1), reverse=True)
+    ids = [t[0] for t in bow_sorted]
+
     # parse bag of words and replace the word index with the dense vector
-    # for the sum of [count in bag of words] * [embedding] for the
-    # top max_words
-    for idx, count in bow.items():
+    # for the sum of [count in bag of words] * [embedding]
+    for idx, count in bow_sorted:
 
-        idx = int(idx)
-
-        if idx < max_words:
+        if ((max_words and global_max and idx < max_words) or
+           (max_words and not global_max and idx in ids[:max_words]) or
+           (max_words is None)):
             word = words[idx]
             embedding = glove[word]
             vector = vector + count * embedding
