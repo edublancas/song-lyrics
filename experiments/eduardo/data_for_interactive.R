779e102c7a7cca467382433822fe97e475b5980f
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-04-03 16:06:00 -0400

    updates cript

--- experiments/eduardo/data_for_interactive.R
+++ experiments/eduardo/data_for_interactive.R
@@ -1,36 +1,52 @@
 library(feather)
 library(dplyr)
 
-# columns to use for grouping: year, genre, artist, duration
-# also use track_id for distances
-# TODO: check artist_name vs artist_id
+# columns to use for grouping: year, genre (only top), artist (only top),
+# duration also use track_id for distances
 
 bow <- read_feather('data/transform/bag_of_words_top_100.feather')
+
+genres_ <- table(bow$genre_)
+
+length(genres_[genres_ >= 10])
+
+
+bow %>% group_by(genre_) %>% summarise(n=n())
+
+sort(table(bow$genre_), decreasing=TRUE)[0:10]
+
+# TODO: check artist_name vs artist_id
+length(unique(bow$artist_id_))
+length(unique(bow$artist_name_))
 
 head(bow)
 colnames(bow)
 
-apply_to_words <- function(df, fn){
-    df_words <- select(df, -ends_with('_'))
-    data.frame(t(fn(df_words)))
+apply_to_words <- function(df, fn, threshold){
+    if(nrow(df) >= threshold){
+        df_words <- select(df, -ends_with('_'))
+        return(data.frame(t(fn(df_words))))
+    }
+
+    return(data.frame())
 }
 
 
 # group and summarize operations (sum, mean)
-artist_sum <- bow %>% group_by(artist_name_) %>% do(apply_to_words(., colSums))
 
 res <- bow %>% group_by(release_year_) %>% do(apply_to_words(., colMeans))
-res_ <- bow %>% group_by(release_year_) %>% do(apply_to_words(., colSums))
 
-res <- bow %>% group_by(genre_) %>% do(apply_to_words(., colSums))
+genre_means <- bow %>% group_by(genre_) %>% do(apply_to_words(., colMeans, 10))
+artist_means <- bow %>% group_by(artist_name_) %>% do(apply_to_words(., colMeans, 10))
 
+dim(res)
 head(res)
 head(res_)
 
 # compute a distances matrix
 distances <- function(df, row_names){
     df <- data.frame(df)
-    df <- df[!is.na(res$release_year_), ]
+    df <- df[!is.na(df[[row_names]]), ]
     rownames(df) <- df[[row_names]]
     m <- as.matrix(dist(df, diag=TRUE, upper=TRUE))
     return(m)
@@ -44,4 +60,29 @@ m['1925', '1924']
 
 closest(m, '1924', 5)
 farest(m, '1924', 5)
+
+
+# distance matrix of artists is too big.. maybe filter by artists with > x
+# songs?
+m_ <- distances(artist_sum, 'artist_name_')
+
+
+m <- distances(genre_means, 'genre_')
+closest(m, 'Rock', 10)
+farest(m, 'Rock', 10)
+
+m <- distances(artist_means, 'artist_name_')
+closest(m, 'Pink Floyd', 20)
+farest(m, 'Pink Floyd', 10)
+
+# top words for artist
+artist <- data.frame(filter(artist_means, artist_name_ == 'Oasis'))
+artist <- as.numeric(artist[1, 2:101])
+names(artist) <- colnames(artist_means[1, 2:101])
+sort(artist, decreasing=TRUE)
+
+
+sort(as.numeric(artist[1, 1:101]))
+
+select(artist, -ends_with('_'))
 
