305be72aae98decb725d3edd667cd80f39d7accf
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-04-12 23:53:37 -0400

    adds language column to track metadata

--- experiments/eduardo/word_dedupe.py
+++ experiments/eduardo/word_dedupe.py
@@ -9,3 +9,12 @@ words = mxm['words']
 "'til" in words
 
 process.extract("cos", words, limit=2, scorer=fuzz.ratio)
+
+c = sorted([w for w in words if w.startswith("'")])
+c
+
+[i for i, w in enumerate(words) if w in c]
+
+sorted([w for w in words if w.endswith("in'")])
+
+sorted([w for w in words if w.endswith("ing")])
