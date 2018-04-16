2a71a9dc24020a93ece57f8b0fc0864f311a5544
Jose Alvarado <jalvarado@graphen.ai>
2018-04-16 11:38:55 -0400

    Uncommented a line on clean_data.sh

--- experiments/jose/clean_data.sh
+++ experiments/jose/clean_data.sh
@@ -1,7 +1,7 @@
  #!/usr/bin/env bash
 
 # Running the R script that wrangles the data
-#Rscript ./experiments/jose/DataWrangling.R
+Rscript ./experiments/jose/DataWrangling.R
 
 # Getting the artist sentiment mean score
 Rscript ./experiments/jose/artist_mean_sentiment.R
